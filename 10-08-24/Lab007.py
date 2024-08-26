de5be1a4922d1f7d532c5a25fa8bc92023365759
Rajyalakshmi1989 <gayathri.vemala@qo-box.com>
2024-08-26 11:06:37 +0530

    Hello

---
+++ 10-08-24/Lab007.py
@@ -0,0 +1,4 @@
+print(max(2,7,0,-1,"Pramod"))
+print(max(10, 23, 45, -1, -2, 100, 1, "Pramod"))
+
+# not support
---
+++ 13-08-24/Lab015.py
@@ -0,0 +1,6 @@
+
+# To find the max - functionc - prebuilt functions
+result = max(3, 4)
+#result = max(3, "pramod") - Data Types
+
+print(result)
