f69e81539608c400f13ad274a24928482311ad10
Rajyalakshmi1989 <gayathri.vemala@qo-box.com>
2024-08-28 16:07:02 +0530

    Hello

---
+++ 27-08-24/FunctionBasic.py
@@ -0,0 +1,61 @@
+def greet():
+    print("Hello, there!!")
+
+
+# calling
+greet()
+greet()
+greet()
+
+# def 123():
+#     pass
+
+
+def _():
+    print("Hello")
+
+
+_()
+
+
+def pramod123():
+    print("Hello pramod123")
+
+
+def h():
+    print("hello hhhhhhhhhhhhh")
+    print("I am part of h function ?")
+
+pramod123()
+h()
+def first_part_last_name():
+    pass  # in future i will complete this functions
+
+# pramoddutta - normalcase
+# pramodDutta  - camelcase
+# pramod_dutta - underscore case - This is what we use.
+
+
+def name_pramod():
+    pass
+
+# Types of Functions in Python
+# 1. Built-in functions - len(), min(), and print() are examples of built-in functions.
+# https://docs.python.org/3/library/functions.html#built-in-funcs
+# 2. User-defined functions
+
+# def and call
+# user definded functions
+
+def greet_to_all_of_you():
+    print("Hello, Everyone")
+
+
+def greetyes():
+    print("Yes grreting")
+    greet_to_all_of_you()
+
+
+greetyes()
+greet_to_all_of_you()
+
