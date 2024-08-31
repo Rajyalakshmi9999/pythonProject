5320136d05d868886b24a78d5ec3bcd793d4409f
Rajyalakshmi1989 <gayathri.vemala@qo-box.com>
2024-08-31 12:52:25 +0530

    31-08-24 Task

---
+++ 31-08-24/EmpConstructor.py
@@ -0,0 +1,37 @@
+
+# Constructor
+# Special Function in Class,  __init__()
+# It will be automatically called when you create an Object
+
+class Employee:
+    name = None
+    eid = None
+    phone = None
+    age = None
+    city = None
+
+    def __init__(self,name,eid,phone,age,city):
+        print("Called, Object is created")
+        self.name = name
+        self.age = age
+        self.eid = eid
+        self.phone = phone
+        self.city = city
+
+
+
+    def display(self):
+        print("data display")
+        print("This is the Employee data -> ", self.name, self.age,self.eid,self.phone,self.city)
+        return None
+
+
+emp11 = Employee("Ram", 10,"abc@gamil",86666,"Bang")
+print(emp11.name)
+print(emp11.age)
+print(emp11.eid)
+print(emp11.city)
+print(emp11.phone)
+
+emp11.display()  # ->
+
